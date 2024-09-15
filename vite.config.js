a825c9d55b8d44783ef1fe195bfb44aee61794cc
Unify Systems <unifysystems@Unifys-MacBook-Pro.local>
2024-09-15 18:02:00 +0530

    Deploy on pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), ghPages()],
+  base: '/git@github.com:vikramsinghshahi/MobileViewYCCA.git/', // Update with your GitHub repository name
+});
