4aba99fc8b9e18d162641ea61eae354ed82c5f50
Unify Systems <unifysystems@Unifys-MacBook-Pro.local>
2024-09-15 18:20:37 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   base: '/MobileViewYCCA/',  
 });
