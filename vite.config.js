f6d6238c8b4d3595afffc7c287c301c6a7123034
Unify Systems <unifysystems@Unifys-MacBook-Pro.local>
2024-09-15 18:28:17 +0530

    Fix public folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
-  base: '/MobileViewYCCA/',  
+  plugins: [react()], 
 });
